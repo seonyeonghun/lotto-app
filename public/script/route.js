5c14f74df1301f23dec9800aeb580b3948634947
seon yeong hun <seonyeonghun@kakao.com>
2022-04-01 12:59:23 +0900

    lotto-app 1차 완성

--- public/script/route.js
+++ public/script/route.js
@@ -20,3 +20,24 @@ function sideOpen(){
 function sideClose(){
     sidePan.classList.add("d-none");
 }
+
+/* 로또 1게임 - 잠금해제 : 내용은 아직 작성안함 */
+const toggleLockBtn = document.querySelector("#blind")
+toggleLockBtn.addEventListener("click", handleClick)
+let btnStatus = "lock"
+
+function handleClick(e){
+    if(e.target.className == btnStatus) {
+        unlock("해제");                        
+    } else {
+        lock("잠금");
+    }
+}
+function unlock(message){
+    toggleLockBtn.className = "unlock"
+    toggleLockBtn.textContent = message
+}
+function lock(message) {
+    toggleLockBtn.className = "lock"
+    toggleLockBtn.textContent = message
+}
