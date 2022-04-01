18ae5043c29300c58dd656ebd9f5fa683916a568
seon yeong hun <seonyeonghun@kakao.com>
2022-04-01 11:05:39 +0900

    lotto-app 번호생성 프로그램 완료

--- public/script/lotto.js
+++ public/script/lotto.js
@@ -5,7 +5,7 @@ const make5num = document.querySelector("#make_num5")
 
 // 이벤트 등록
 make1num.addEventListener("click", show1num);
-// make5num.addEventListener("click", show5num);
+make5num.addEventListener("click", show5num);
 
 // 로또 생성 함수
 function show1num(){
@@ -19,18 +19,48 @@ function show1num(){
     //console.log(num_one); // 확인한다
     // 추출   .slice()  vs .splice()
 
-    let start = Math.ceil(Math.random() * 40); // 정수로 시작점
+    // 정수로 시작점
+    let start = Math.ceil(Math.random() * 40); 
     let new_num_one = num_one.slice(start, start+6);
     new_num_one.sort(function(a,b){ // 오름차순정리
         return a-b;
-    })   
+    })
     localStorage.setItem("lottoNum",new_num_one)
     
     location.href = "/result1"
 }
 
 
-// function show5num(){
+function show5num(){
+    let num_five = [];
+    for(let i=1; i<46; i++){ // 1~45 만든다
+        num_five.push(i);
+    }
+    // console.log(num_five); // 최초 만들어진 1~45 순서대로 
     
+    // 일반함수 표기법
+    // num_five.sort(function(a,b){
+    //     return Math.random() - 0.7
+    // });
+
+    // arrow function (화살표 함수 표기법)
+    num_five.sort((a,b)=>{return Math.random() - 0.7}); 
     
-// }
+    // 6개씩 랜덤 5회 추출
+    let num_five_arr = [];
+    for(let i=0; i<5; i++){
+        let start = Math.ceil(Math.random() * 40);   
+        // console.log("start : "+start)
+        let new_num_five = num_five.slice(start, start+6);
+        new_num_five.sort(function(a,b){ // 오름차순정리
+            return a-b;
+        });
+        // console.log(new_num_five);
+        num_five_arr.push(new_num_five);
+    }
+    //console.log(num_five_arr);
+
+    // JSON.stringfigy()  vs  JSON.parse() : 데이터 주고받을때 사용
+    localStorage.setItem("lottoNums",JSON.stringify(num_five_arr))
+    location.href = "/result5"
+}
